fe785715e3bf2886a566046c3ca030d786fec481
YuanShenjian <sjyuan@thoughtworks.com>
2018-06-15 16:47:58 +0800

    Update setup script

--- ci/startup-jenkins.sh
+++ ci/startup-jenkins.sh
@@ -6,6 +6,6 @@ docker run \
   -d \
   -p 8080:8080 \
   -p 50000:50000 \
-  -v jenkins-data:/var/jenkins_home \
+  -v /var/jenkins_home:/var/jenkins_home \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  nho/jenkins-ci
+  jenkinsci/blueocean
