7a4d1a1ca2bf3fd056a54926d390049df54bf24a
YuanShenjian <sjyuan@thoughtworks.com>
2018-07-04 14:29:45 +0800

    Add install docker shell script
    Signed-off-by: YuanShenjian <sjyuan@thoughtworks.com>

---
+++ ci/install-docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+
+# Use docker withou sudo, after setting, relogin to the server
+sudo usermod -aG docker ${USER}
